669ae9bcacc74c3ad6d97a0e3cc8fadc67ddfe63
SasinduPrasad <sasinduprasad2002@gmail.com>
2025-03-19 22:14:33 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.sasinduprasad.pockethost"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
